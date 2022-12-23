e8331790904d9362b9df5539569c0675ecd278ab
khoatrinh <khoatrinh2108@gmail.com>
2022-12-23 18:25:25 -0500

    Added logic to create new Excel sheet if not existed

--- Models/ExcelBook.py
+++ Models/ExcelBook.py
@@ -1,8 +1,14 @@
-from openpyxl import load_workbook
+from openpyxl import load_workbook, Workbook
 
 class ExcelBook:
     def __init__(self, path):
         self.path = path
-        self.book = load_workbook(path)
+        try:
+            self.book = load_workbook(path)
+            print("Opening existing Excel file: ", path)
+        except:
+            self.book = Workbook()
+            self.book.save(path)
+            print("Creating new Excel file: ", path)
 
 
